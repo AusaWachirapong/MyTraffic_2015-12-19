d883fc35c53272b365b9410be7d34f30ccb00571
Wachirapong <wachirapong.kamutchat@gmail.com>
2015-12-19 15:02:59 +0700

    Finish 1 st Day

--- app/src/main/java/development/kamutchat/wachirapong/mytraffic/MainActivity.java
+++ app/src/main/java/development/kamutchat/wachirapong/mytraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package development.kamutchat.wachirapong.mytraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
                 MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_shut);
                 buttonMediaPlayer.start();
 
+                // Intent to Web View
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/zYNsQ6ibdZs"));
+
+                startActivity(objIntent);
             }   //event
         });
     }
