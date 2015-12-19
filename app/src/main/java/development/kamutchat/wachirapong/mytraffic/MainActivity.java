4561e0b14b6664af079c721c547a002ae4fcc3ae
Wachirapong <wachirapong.kamutchat@gmail.com>
2015-12-19 14:46:55 +0700

    SoundEffect Finish

--- app/src/main/java/development/kamutchat/wachirapong/mytraffic/MainActivity.java
+++ app/src/main/java/development/kamutchat/wachirapong/mytraffic/MainActivity.java
@@ -1,13 +1,50 @@
 package development.kamutchat.wachirapong.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        // Button Controller
+        buttonController();
+
+
+    }   // Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_shut);
+                buttonMediaPlayer.start();
+
+            }   //event
+        });
     }
-}
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    }
+
+}   // Man Class
