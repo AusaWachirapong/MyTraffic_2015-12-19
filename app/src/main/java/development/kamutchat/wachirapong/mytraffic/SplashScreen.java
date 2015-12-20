915619cef6527ed78f35d2ec7768651f993c66d5
Wachirapong <wachirapong.kamutchat@gmail.com>
2015-12-20 14:47:30 +0700

    All Finish First Step

---
+++ app/src/main/java/development/kamutchat/wachirapong/mytraffic/SplashScreen.java
@@ -0,0 +1,30 @@
+package development.kamutchat.wachirapong.mytraffic;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },7000);
+
+
+        MediaPlayer introMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_start_horse);
+        introMediaPlayer.start();
+    }   //Main Method
+
+}   //Main Class
